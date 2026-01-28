ddb617b3a919729e448dfd1f6a34a1f0654de718
Manas-2007 <patidarmanas01@gmail.com>
2026-01-28 12:53:56 +0530

    Lec 1 Basics of Python

---
+++ division.py
@@ -0,0 +1,13 @@
+#Single division (/) always provides output as FLOATING value
+#Double Division (//) always provides output as INTEGER value
+
+a=15
+b=5
+print(a/b)
+print(a//b)
+
+a=5
+b=15
+print(a/b)
+print(a//b)
+
---
+++ learn.py
@@ -0,0 +1,6 @@
+a,b=15,5
+x=a//b
+print(x)
+a,b=5,15
+x=a//b
+print(x)
