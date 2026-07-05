47c09f463e4a3ab0ea38fc0f3490ae42a1603094
Manas-2007 <patidarmanas01@gmail.com>
2026-07-05 10:09:08 +0530

    OOps in python basics

---
+++ 09_OOPs/test.py
@@ -0,0 +1,28 @@
+class Car:
+
+    # Parameterized constructor
+    def __init__(self,brand,model):
+        self.brand=brand;
+        self.model=model;
+    
+    # Member function 
+    def Display(self):
+        print("Brand :",self.brand);
+        print("Model :",self.model);
+
+#Inheritance
+class Electric_Car(Car):
+    def __init__(self,brand,model,battery_size):
+        super().__init__(brand,model);
+        self.battery_size=battery_size;
+
+    def Display(self):
+        super().Display();
+        print("Battery Size :",self.battery_size);
+
+
+
+
+# Creating object
+my_Electric=Electric_Car("Ola","Scooty","400 volt");
+my_Electric.Display();
