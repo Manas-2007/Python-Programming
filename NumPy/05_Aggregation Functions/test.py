075136c7157d23d2b5a233c4cbfdbfee6f6e1411
Manas-2007 <patidarmanas01@gmail.com>
2026-07-15 10:01:57 +0530

    started Sorting + filtering

---
+++ NumPy/05_Aggregation Functions/test.py
@@ -0,0 +1,16 @@
+import numpy as np
+data=np.array([[34,56,12],[23,45,67],[89,12,34]])
+
+# Sum of all elements in the array
+total_sum=np.sum(data)
+print("Sum of all elements:", total_sum)
+
+# Mean of all elements in the array
+mean_value=np.mean(data)
+print("Mean of all elements:", mean_value)
+
+# Maximum value in the array
+max_value=np.max(data);
+print("Maximum value in the array :",max_value);
+
+
---
+++ NumPy/06_Sorting & Filtering/test.py
@@ -0,0 +1,20 @@
+import numpy as np
+vector=np.array([3,6,89,6,5,34,67,54,6,34])
+
+# # Sorting of Data
+sorted_data=np.sort(vector)
+print("1D Data :\n",sorted_data)
+
+matrix=np.array([[1,2,6],[8,6,3],[9,4,3]])
+matrix_sorted=np.sort(matrix,axis=0)
+print("2D Matrix :\n",matrix_sorted)
+
+
+# # Filtering of Data
+greater=vector>60
+print(vector[greater])
+
+# WHERE Clause in NumPy (Exactly same as ternary operator in C++)
+data1=np.array([12,45,32,76,89,4,6,5,2,3,65,67,66,44,43,31,79])
+filter_update=np.where(data1>50 , data1*(-1) , data1)
+print("Where Clause Result :\n",filter_update)
