70419ed2d38fa761ea1e81d0fc40a241a2f76878
Manas-2007 <patidarmanas01@gmail.com>
2026-07-17 13:21:52 +0530

    Started Formatting in Matrix

--- NumPy/06_Sorting & Filtering/test.py
+++ NumPy/06_Sorting & Filtering/test.py
@@ -1,16 +1,19 @@
 import numpy as np
 vector=np.array([3,6,89,6,5,34,67,54,6,34])
 
-# # Sorting of Data
+# Sorting of Data
 sorted_data=np.sort(vector)
 print("1D Data :\n",sorted_data)
 
 matrix=np.array([[1,2,6],[8,6,3],[9,4,3]])
 matrix_sorted=np.sort(matrix,axis=0)
-print("2D Matrix :\n",matrix_sorted)
+print("Row Wise Sort :\n",matrix_sorted)
+
+matrix_sorted=np.sort(matrix,axis=1)
+print("Column Wise Sort :\n",matrix_sorted)
 
 
-# # Filtering of Data
+# Filtering of Data
 greater=vector>60
 print(vector[greater])
 
@@ -18,3 +21,4 @@ print(vector[greater])
 data1=np.array([12,45,32,76,89,4,6,5,2,3,65,67,66,44,43,31,79])
 filter_update=np.where(data1>50 , data1*(-1) , data1)
 print("Where Clause Result :\n",filter_update)
+ 
